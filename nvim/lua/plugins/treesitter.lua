faac83587e265d3f68dba7d6b1838e151e79d15d
s1mple <mrzkk07@gmail.com>
2025-02-22 15:55:25 +0530

    first commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      ensure_installed = {
+        "rust",
+        "go",
+        "zig",
+        "c",
+        "cpp",
+        "css", -- CSS
+        "javascript", -- JavaScript
+        "python", -- Python
+        "sql", -- SQL
+        "html", -- HTML
+        "markdown", -- Markdown
+        "lua", -- Lua
+        "bash", -- Bash
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
